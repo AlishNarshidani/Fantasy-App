bb86dbaefbf42a0e715b9a2e01270e3bdea48472
Mihir Dhami <mihirdhami745@gmail.com>
2024-08-10 10:53:52 +0530

    User profile backend

--- app/src/main/java/com/example/fantasyapp/ProfiePage.java
+++ app/src/main/java/com/example/fantasyapp/ProfiePage.java
@@ -14,29 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProfiePage extends AppCompatActivity {
 
-    Button userButton,walletButton,rulesButton,contectButton,termButton,logoutButton;
-
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profie_page);
-            userButton = findViewById(R.id.userButton);
-            walletButton = findViewById(R.id.walletButton);
-            rulesButton = findViewById(R.id.rulesButton);
-            contectButton = findViewById(R.id.contectButton);
-            termButton = findViewById(R.id.termsButton);
-            logoutButton = findViewById(R.id.logutButton);
 
-        rulesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an Intent to start SecondActivity
-                Intent intent = new Intent(ProfiePage.this, FantsyPoint.class);
-                startActivity(intent);
-            }
-        });
+
 
     }
 }
