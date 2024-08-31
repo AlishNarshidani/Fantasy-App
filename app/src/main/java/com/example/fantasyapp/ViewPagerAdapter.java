2925c440a4029eb12d46a7127368e22cd0cf0a28
Swapnil <swapnilverma00007@gmail.com>
2024-09-01 01:55:13 +0530

    Upcoming

---
+++ app/src/main/java/com/example/fantasyapp/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.fantasyapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    private ArrayList<Match> liveMatches;
+    private ArrayList<Match> upcomingMatches;
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity, ArrayList<Match> liveMatches, ArrayList<Match> upcomingMatches) {
+        super(fragmentActivity);
+        this.liveMatches = liveMatches;
+        this.upcomingMatches = upcomingMatches;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new LiveMatchesFragment(liveMatches);
+            case 1:
+                return new UpcomingMatchesFragment(upcomingMatches);
+            default:
+                return new LiveMatchesFragment(liveMatches);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
