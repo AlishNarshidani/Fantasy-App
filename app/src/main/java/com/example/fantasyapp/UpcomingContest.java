4968450a3267364732abd7821170250ccada1340
AlishNarshidani <alishnarshidani586@gmail.com>
2024-10-14 23:20:57 +0530

    setting up upcoming contests

--- app/src/main/java/com/example/fantasyapp/UpcomingContest.java
+++ app/src/main/java/com/example/fantasyapp/UpcomingContest.java
@@ -6,16 +6,42 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UpcomingContest extends AppCompatActivity {
 
     AppCompatButton createTeam, viewTeams;
     Match match;
+
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+
+    List<Map<String, Object>> contestsList;
+
+    RecyclerView recyclerView;
+    contestAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +54,17 @@ public class UpcomingContest extends AppCompatActivity {
             return insets;
         });
 
+        recyclerView = findViewById(R.id.displayAllContestRecyclerView);
 
         createTeam = findViewById(R.id.createTeam);
         viewTeams = findViewById(R.id.viewTeams);
+        db = FirebaseFirestore.getInstance();
+        auth= FirebaseAuth.getInstance();
+        contestsList = new ArrayList<>();
 
         match = (Match) getIntent().getSerializableExtra("match");
+
+        checkAvailableContests();
 
         createTeam.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,5 +83,103 @@ public class UpcomingContest extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    public void checkAvailableContests()
+    {
+        db.collection("contests")
+                .whereEqualTo("match_id",match.getId())
+                .whereEqualTo("contestType","public")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            if (!task.getResult().isEmpty()) {
+
+                                contestsList.clear();
+
+                                // At least one public contest is available for the match
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                    // Get contest data as a map and add to the list
+                                    Map<String, Object> contestData = document.getData();
+                                    contestsList.add(contestData);
+                                }
+
+                                loadContests();
+                                Log.d("Contests", "Fetched contests: " + contestsList);
+
+                            } else {
+                                // No public contest is available for the match
+                                Log.d("PublicContest", "No public contests available for the match.");
+
+                                createDefaultPublicContest();
+
+                            }
+                        } else {
+                            // Handle the error
+                            Log.d("FirestoreError", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void createDefaultPublicContest()
+    {
+        String matchId = match.getId();
+        String userId = auth.getUid();
+
+        Map<String, Object> newContest = new HashMap<>();
+        newContest.put("contestType", "public");
+        newContest.put("match_id", matchId);
+        newContest.put("team_ids", new ArrayList<String>()); // Add an empty team ID list (or initialize it with default teams)
+        newContest.put("entry_fee", 50);  // Default entry fee
+        newContest.put("prize_pool", 2400);  // Default prize pool
+        newContest.put("max_teams", 50);  // Default max teams allowed
+        newContest.put("numberOfWinners",3);
+
+        // Add the contest document to Firestore
+
+        db.collection("contests")
+                .add(newContest)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("PublicContest", "Default public contest created with ID: " + documentReference.getId());
+
+                        //added document id as the contest id
+                        documentReference.update("contest_id", documentReference.getId())
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("PublicContest", "Document ID added as a field successfully");
+                                        // Reload or update your contests view if needed
+                                        checkAvailableContests();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d("FirestoreError", "Error updating document with contest ID", e);
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("FirestoreError", "Error creating default public contest", e);
+                    }
+                });
+    }
+
+    public void loadContests()
+    {
+        adapter = new contestAdapter(UpcomingContest.this,contestsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/fantasyapp/contestAdapter.java
@@ -0,0 +1,98 @@
+package com.example.fantasyapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.Map;
+
+public class contestAdapter extends RecyclerView.Adapter<contestAdapter.contestViewHolder> {
+
+    private Context context;
+    private List<Map<String, Object>> contestsList;
+
+    public contestAdapter(Context context, List<Map<String, Object>> contestsList) {
+
+        this.context = context;
+        this.contestsList = contestsList;
+    }
+
+    @NonNull
+    @Override
+    public contestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contest_layout, parent, false);
+        return new contestAdapter.contestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull contestViewHolder holder, int position) {
+        Map<String, Object> contest = contestsList.get(position);
+
+        // Bind data to the views in the contestViewHolder
+        String contestTypeStr = contest.get("contestType").toString();
+        String prizePoolStr = contest.get("prize_pool").toString();
+        String totalSpotsStr = contest.get("max_teams").toString();
+        String matchIdStr = contest.get("match_id").toString();
+        String contestIdStr = contest.get("contest_id").toString();
+        List<String> teamIds = (List<String>) contest.get("team_ids");
+        String entryFeeStr = contest.get("entry_fee").toString();
+        String numberOfWinnersStr = contest.get("numberOfWinners").toString();
+
+        int firstPrizeInt = Integer.parseInt(prizePoolStr) / 2;
+
+        int numberOfRegisteredTeams = teamIds != null ? teamIds.size() : 0;
+
+        Log.d("teamRegistered", "onBindViewHolder: "+numberOfRegisteredTeams);
+
+
+        int spotsLeftInt = Integer.parseInt(totalSpotsStr) - numberOfRegisteredTeams;
+
+        int totalSpotsInt = Integer.parseInt(totalSpotsStr);
+        int numberOfWinnersInt = Integer.parseInt(numberOfWinnersStr);
+
+        int winPerc = (numberOfWinnersInt/totalSpotsInt)*100;
+
+        holder.prizePool.setText("₹"+prizePoolStr);
+        holder.firstPrize.setText("₹"+String.valueOf(firstPrizeInt));
+        holder.spotsLeft.setText(String.valueOf(spotsLeftInt)+" spots left");
+        holder.totalSpots.setText(totalSpotsStr+" spots");
+        holder.entryFeesBtn.setText("₹"+entryFeeStr);
+        holder.spotsFilledBar.setMax(totalSpotsInt);
+        holder.spotsFilledBar.setProgress(numberOfRegisteredTeams, true);
+        holder.winningPercentage.setText(String.valueOf(winPerc)+"%");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contestsList.size();
+    }
+
+    static class contestViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView prizePool, spotsLeft, totalSpots, firstPrize, winningPercentage;
+        AppCompatButton entryFeesBtn;
+        ProgressBar spotsFilledBar;
+
+        public contestViewHolder(@NonNull View itemView) {
+            super(itemView);
+            prizePool = itemView.findViewById(R.id.prizePool);
+            spotsLeft = itemView.findViewById(R.id.spotsLeft);
+            totalSpots = itemView.findViewById(R.id.totalSpots);
+            firstPrize = itemView.findViewById(R.id.firstPrize);
+            winningPercentage = itemView.findViewById(R.id.winningPercentage);
+
+            entryFeesBtn = itemView.findViewById(R.id.entryFeesBtn);
+            spotsFilledBar = itemView.findViewById(R.id.spotsFilledBar);
+        }
+    }
+}
